412f88d4297fc2400201a3fb0f0caa35d5bb7cb8
Havennnn <latrellmarable00@gmail.com>
2025-11-02 14:04:33 +0800

    chore: added route version 1

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::prefix('v1')->name('api.v1')->group(function () {
+    //
+
+    Route::middleware('auth:sanctum')->group(function () {
+        //
+    });
+});
