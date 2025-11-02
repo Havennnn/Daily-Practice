7ed4d9d56ef634d8dee29487ee4e152109f8d8ec
Havennnn <latrellmarable00@gmail.com>
2025-11-02 15:29:33 +0800

    refactor: changes lease to use apiResource

--- routes/api.php
+++ routes/api.php
@@ -9,23 +9,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
     Route::middleware('throttle:5,1')->group(function () {
-        Route::post('/register', [AuthController::class, 'register']);
-        Route::post('/login', [AuthController::class, 'login']);
+        Route::post('/register', [AuthController::class, 'register'])->name('register');
+        Route::post('/login', [AuthController::class, 'login'])->name('login');
     });
 
     Route::middleware('auth:sanctum')->group(function () {
-        Route::post('/logout', [AuthController::class, 'logout']);
-
-        /* Route::get('/tickets', [TicketController::class, 'index']);
-        Route::post('/tickets', [TicketController::class, 'store']);
-        Route::get('/tickets/{ticket}', [TicketController::class, 'show']);
-        Route::put('/tickets/{ticket}', [TicketController::class, 'update']);
-        Route::delete('/tickets/{ticket}', [TicketController::class, 'destroy']); */
-
+        Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
         Route::apiResource('tickets', TicketController::class);
-        Route::put('/tickets/{ticket}/lease', [TicketLeaseController::class, 'lease']);
-        Route::put('/tickets/{ticket}/unlease', [TicketLeaseController::class, 'unlease']);
-        Route::post('/tickets/{ticket}/comments', [CommentController::class, 'store']);
-        Route::get('/tickets/{ticket}/comments', [CommentController::class, 'index']);
+        Route::apiResource('tickets.comments', CommentController::class)->only(['index','store']);
+        Route::put('/tickets/{ticket}/lease', [TicketLeaseController::class, 'lease'])->name('tickets.lease');
+        Route::put('/tickets/{ticket}/unlease', [TicketLeaseController::class, 'unlease'])->name('tickets.unlease');
     });
 });
